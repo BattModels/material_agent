a029c8511c1c5b44004dd36802bd78509a40a3e9
Hongshuo Huang <115762837+hongshuh@users.noreply.github.com>
2024-09-02 19:58:06 -0400

    fix get bulk modules

--- invoke.py
+++ invoke.py
@@ -6,10 +6,15 @@ from langchain_core.messages import (
     HumanMessage,
     ToolMessage,
 )
+from langchain_anthropic import ChatAnthropic
+
+from langgraph.prebuilt import create_react_agent
+from src.prompt import *
 from src.graph import create_graph
 
 
 from src.utils import load_config, save_graph_to_file
+from src.tools import *
 
 
 def _set_if_undefined(var: str):
@@ -21,6 +26,7 @@ if __name__ == "__main__":
     
     config = load_config(os.path.join('./config', "default.yaml"))
     WORKING_DIRECTORY = config['working_directory']
+    pseudo_dir = config['pseudo_dir']
     os.makedirs(WORKING_DIRECTORY, exist_ok=True)
 
     _set_if_undefined("ANTHROPIC_API_KEY")
@@ -29,7 +35,10 @@ if __name__ == "__main__":
     _set_if_undefined("LANGSIM_MODEL")
 
     graph = create_graph(config)
-    
+    # llm = ChatAnthropic(model=config["LANGSIM_MODEL"], api_key=config['ANTHROPIC_API_KEY'])
+
+    # graph = create_react_agent(llm, tools=[get_kpoints, dummy_structure, find_pseudopotential,write_script,get_bulk_modulus],
+    #                                state_modifier=dftwriter_prompt)
 
     save_graph_to_file(graph, WORKING_DIRECTORY)
     # exit()
@@ -50,11 +59,22 @@ if __name__ == "__main__":
     #     print("----")
 
 
+#     for s in graph.stream(
+#     {
+#         "messages": [
+#             HumanMessage(content=f"Generate a quantum espresso input for a crystal structure with 100% Cu atoms and calculate its bulk modules. The working directory is {WORKING_DIRECTORY}.\
+#                         The pseduopotential directory is {pseudo_dir}.")
+#         ]
+#     }
+# ):
+#         if "__end__" not in s:
+#             print(s)
+#             print("----")
     for s in graph.stream(
     {
         "messages": [
-            HumanMessage(content=f"Generate a quantum espresso input for a crystal structure with 100% Cu atoms and save into a file. The working directory is {WORKING_DIRECTORY}.\
-                        The pseduopotential directory is {config['pseudo_dir']}. The k point distance is normal.")
+            HumanMessage(content=f"Generate a quantum espresso input for a crystal structure with 100% Cu atoms and calculate its bulk modules. The working directory is {WORKING_DIRECTORY}.\
+                        The pseduopotential directory is {pseudo_dir}.")
         ]
     }
 ):
