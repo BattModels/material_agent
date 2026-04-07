70591ab7277bde8da6213cbf8af997982f1e4c66
Matminator <121713591+Matminator@users.noreply.github.com>
2026-04-07 19:03:55 +0200

    version req check for GNoME_DREAMS_OER_screening package

--- invoke.py
+++ invoke.py
@@ -33,7 +33,13 @@ from pathlib import Path
 from gnome_dreams_oer_screening.explog.explog import EXPLOG
 
 
+# --- Ensure the new GNoME_DREAMS_OER_screening package meets the minimum version requirement ------
+from importlib.metadata import version
+from packaging.version import Version
 
+if Version(version("GNoME_DREAMS_OER_screening")) < Version("0.5.4"):
+    raise RuntimeError("GNoME_DREAMS_OER_screening>=0.5.4 is required")
+# --- End of version check -------------------------------------------------------------------------
 
 if __name__ == "__main__":
 
